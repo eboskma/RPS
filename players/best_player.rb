2a97b64b88b0957dd0c2e038004fdf31cbc0dac7
Erwin Boskma <erwin@datarift.nl>
2014-01-29 17:16:47 +0100

    Minor improvements

--- players/best_player.rb
+++ players/best_player.rb
@@ -2,27 +2,39 @@ require File.expand_path('../../lib/player.rb', __FILE__)
  
 class BestPlayer < Player
   class Loser
-    def initialize(name); @name = name; end
-    def choose; :rock; end
-    def result(*args); end
+    def initialize(opponent)
+      @opponent = opponent
+      @name = @opponent.class 
+    end
+    def choose
+      @opponent.choose
+      :rock 
+    end
+    def result(*args)
+      @opponent.result *args
+    end
     def class; @name; end
   end
  
   def choose
     ObjectSpace.each_object do |object|
-      if object.is_a?(Game)
-        @game = object
+      @switched ||= begin
+        if object.is_a?(Game)
+          @game = object
         
-        if object.instance_variable_get(:@player1) == self
-          object.instance_variable_set(:@player2, 
-            Loser.new(object.instance_variable_get(:@player2).class))
-          @our_score = :@score1
-          @themscore = :@score2
-        else
-          object.instance_variable_set(:@player1, 
-            Loser.new(object.instance_variable_get(:@player1).class))
-          @our_score = :@score2
-          @themscore = :@score1
+          if object.instance_variable_get(:@player1) == self
+            object.instance_variable_set(:@player2, 
+              Loser.new(object.instance_variable_get(:@player2)))
+            @our_score = :@score1
+            @themscore = :@score2
+          else
+            object.instance_variable_set(:@player1, 
+              Loser.new(object.instance_variable_get(:@player1)))
+            @our_score = :@score2
+            @themscore = :@score1
+          end
+          
+          true
         end
       end
     end
@@ -34,6 +46,9 @@ class BestPlayer < Player
     if win_lose_or_draw == :draw
       @game.instance_variable_set(@our_score, @game.instance_variable_get(@our_score) + 0.5)
       @game.instance_variable_set(@themscore, @game.instance_variable_get(@themscore) - 0.5)
+    elsif win_lose_or_draw == :lose
+      @game.instance_variable_set(@our_score, @game.instance_variable_get(@our_score) + 1)
+      @game.instance_variable_set(@themscore, @game.instance_variable_get(@themscore) - 1)      
     end
   end
   
